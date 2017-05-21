35e353e0f1a4955fc600ce03e81919f52e0b9352
Nikodem Kowalec <nkowalec@hotmail.com>
2017-05-21 19:01:20 +0200

    Wstępny commit po części początkowych prac nad ekranem głównym

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+function includeAll(){
+  var elems = document.getElementsByTagName('*');
+  for (var i = 0; i < elems.length; i++) {
+    var e = elems[i];
+    let attr = e.getAttribute('data-include');
+    if(attr){
+      req = new XMLHttpRequest();
+      req.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+          e.innerHTML = this.responseText;
+          e.removeAttribute('data-include');
+          includeAll();
+          clickConvert();
+        }
+      }
+      req.open("GET", attr, true);
+      req.send();
+      return;
+    }
+  }
+}
+
+function getBase(){
+  var base = document.getElementsByTagName('base')[0];
+  return base.getAttribute('href');
+}
+
+function clickConvert(){
+  var elems = document.getElementsByTagName('*');
+  for(var i = 0; i < elems.length; i++) {
+    var e = elems[i];
+    let attr = e.getAttribute('click-href');
+    if(attr){
+      if(!attr.startsWith('http')){
+        attr = getBase() + attr;
+      }
+      e.onclick = function() { window.location.href = attr; };
+      e.removeAttribute('click-href');
+    }
+  }
+}
+
+
+includeAll();
+clickConvert();
